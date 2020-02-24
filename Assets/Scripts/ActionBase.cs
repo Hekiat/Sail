6a6e0fedf26d697b69371d927d32b6ff381e62c1
Hekiat <lesueurv@gmail.com>
2020-02-24 12:48:13 +0900

    Update Unity version, added Action configuration system

--- Assets/Scripts/ActionBase.cs
+++ Assets/Scripts/ActionBase.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Assertions;
 
-public class PluginSlot
+public class ActionSlot
 {
     ActionBase function = null;
 
@@ -19,40 +20,94 @@ public class VariableSlot
     bool isEmpty { get { return variable == null; } }
 }
 
-public class VariableBase
+public abstract class VariableBase
 {
-    int value = 5;
 }
 
-public enum FunctionID
+public class IntVariable : VariableBase
 {
+    public int value = 0;
+}
+
+public class FloatVariable : VariableBase
+{
+    public float value = 0f;
+}
+
+public enum VariableID
+{
+    INT,
+    FLOAT,
+    COUNT
+}
+
+public enum ActionID
+{
+    UNDEFINED,
     MOVE,
     ATTACK,
     COUNT
 }
 
-public class ActionBase
+
+public abstract class ActionBase
 {
-    FunctionID FunctionID = FunctionID.MOVE;
+    public string Name { get; private set; } = "Action Name";
 
-    // As a main function
-    List<PluginSlot> FunctionSlots = new List<PluginSlot>();
-    List<VariableBase> FunctionVariableSlots = new List<VariableBase>();
+    // As an action
+    List<ActionSlot> ActionSlots = new List<ActionSlot>();
+    List<VariableBase> VariableSlots = new List<VariableBase>();
 
-    // As a plugin
-    List<VariableBase> PluginVariableSlots = new List<VariableBase>();
+    // As a slotted action
+    List<VariableBase> SecondaryVariableSlots = new List<VariableBase>();
+
+    public virtual void configure(ActionBaseConfiguration config)
+    {
+        Assert.IsNotNull(config, "Action Configuration is null. Type: " + GetType());
+
+        Name = config.Name;
+
+        for (int i = 0; i < config.ActionSlot; i++)
+        {
+            ActionSlots.Add(new ActionSlot());
+        }
+
+        for (int i = 0; i < config.VariableSlot; i++)
+        {
+            VariableSlots.Add(new FloatVariable());
+        }
+
+        for (int i = 0; i < config.SecondaryVariableSlot; i++)
+        {
+            SecondaryVariableSlots.Add(new FloatVariable());
+        }
+    }
 }
 
-[CreateAssetMenu(fileName = "Data", menuName = "Custom/Function", order = 1)]
-public class FunctionClass : ScriptableObject
+[CreateAssetMenu(fileName = "ActionListConfiguration", menuName = "Custom/ActionListConfiguration", order = 0)]
+public class ActionListConfiguration : ScriptableObject
 {
-    public string Name = "New MyScriptableObject";
-
-    public FunctionID FunctionID = FunctionID.MOVE;
-
-    public int FunctionSlot = 0;
-
-    public int FunctionVariableSlot = 0;
-
-    public int PluginVariableSlot = 0;
+    public List<ActionBaseConfiguration> ActionList = null;
 }
+
+public abstract class ActionBaseConfiguration : ScriptableObject
+{
+    public ActionID ActionID = ActionID.UNDEFINED;
+
+    public string Name = "ActionName";
+
+    public int ActionSlot = 0;
+
+    public int VariableSlot = 0;
+
+    public int SecondaryVariableSlot = 0;
+}
+
+
+[CreateAssetMenu(fileName = "MoveConfiguration", menuName = "Custom/MoveConfiguration", order = 2)]
+public class MoveConfiguration : ActionBaseConfiguration
+{
+    public int MoveRange = 3;
+}
+
+
---
+++ Assets/Scripts/ActionDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+
+public class ActionDatabase
+{
+
+    /// List of all ActionID / ActionType dictionnary
+    static List<Tuple<ActionID, Type>> ActionTypes = new List<Tuple<ActionID, Type>>()
+    {
+        new Tuple<ActionID, Type>(ActionID.MOVE, typeof(MoveAction))
+    };
+}
