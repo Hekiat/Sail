2e7d78c8e26d42431a1e0765524e65f8c5409285
Vincent Lesueur <lesueurv@gmail.com>
2020-11-08 09:50:12 +0900

    Update project version and add a basic localisation system

--- Assets/Scripts/GameFlowManager.cs
+++ Assets/Scripts/GameFlowManager.cs
@@ -13,6 +13,7 @@ namespace sail
 
         void Start()
         {
+            LocalizationSystem.Init();
         }
 
 
---
+++ Assets/Scripts/UI/TextLocaliserUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+namespace sail
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TextLocaliserUI : MonoBehaviour
+    {
+        TextMeshProUGUI TextField = null;
+
+        public string key = string.Empty;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            TextField = GetComponent<TextMeshProUGUI>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var value = LocalizationSystem.getLocalisedValue(key);
+            TextField.text = value;
+        }
+    }
+}
