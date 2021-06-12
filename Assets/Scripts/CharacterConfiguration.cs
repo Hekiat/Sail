89931321aba5f87e9bf83259646d583eb1154c1a
Hekiat <lesueurv@gmail.com>
2021-06-12 23:04:08 +0900

    Add Character configurations

---
+++ Assets/Scripts/CharacterConfiguration.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace sail
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "CharacterConfiguration", menuName = "Custom/CharacterConfiguration", order = 1)]
+    public class CharacterConfiguration : ScriptableObject
+    {
+        public CharacterID ID = CharacterID.Invalid;
+
+        public LocalizedString Name = null;
+    }
+}
---
+++ Assets/Scripts/CharacterID.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterID
+{
+    Invalid,
+    ch000,
+    ch001
+}
